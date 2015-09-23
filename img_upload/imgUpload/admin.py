9e8653eabcafc45103191037de5fe12ba4a9db7b
Nicholas Lui <nicholasluimy@gmail.com>
2015-09-23 17:18:23 +0800

    redone, based off http://stackoverflow.com/questions/5871730/need-a-minimal-django-file-upload-example

--- img_upload/imgUpload/admin.py
+++ img_upload/imgUpload/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from img_upload.imgUpload.models import Document
+admin.site.register(Document)
 # Register your models here.
--- img_upload/imgUpload/views.py
+++ img_upload/imgUpload/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from img_upload.imgUpload.models import Document
+from img_upload.imgUpload.forms import DocumentForm
+
+def list(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('img_upload.imgUpload.views.list'))
+    else:
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render_to_response(
+        'list.html',
+        {'documents': documents, 'form': form},
+        context_instance=RequestContext(request)
+    )
